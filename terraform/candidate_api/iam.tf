138fbeed5cf6baedb26bf2e36a27d05826d43b02
Mohammadreza MANSOUR <56890830+mohammadrezamansour@users.noreply.github.com>
2026-06-29 10:54:28 +0200

    Add iam role for lambda

---
+++ terraform/candidate_api/iam.tf
@@ -0,0 +1,98 @@
+data "aws_iam_policy_document" "lambda_assume_role" {
+
+  statement {
+
+    effect = "Allow"
+
+    actions = [
+      "sts:AssumeRole"
+    ]
+
+    principals {
+
+      type = "Service"
+
+      identifiers = [
+        "lambda.amazonaws.com"
+      ]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "lambda_policy" {
+
+  statement {
+
+    sid = "CloudWatchLogs"
+
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+
+    sid = "S3Access"
+
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject"
+    ]
+
+    resources = [
+      module.s3["resume"].bucket_arn,
+      "${module.s3["resume"].bucket_arn}/*"
+    ]
+  }
+
+  statement {
+
+    sid = "DynamoDBAccess"
+
+    effect = "Allow"
+
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:UpdateItem"
+    ]
+
+    resources = [
+      module.dynamodb["candidates"].table_arn
+    ]
+  }
+}
+
+resource "aws_iam_role" "lambda" {
+
+  name = "candidate-api-lambda-role"
+
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+
+  tags = local.default_tags
+}
+
+
+resource "aws_iam_policy" "lambda" {
+
+  name = "candidate-api-lambda-policy"
+
+  policy = data.aws_iam_policy_document.lambda_policy.json
+
+  tags = local.default_tags
+}
+
+resource "aws_iam_role_policy_attachment" "lambda" {
+
+  role = aws_iam_role.lambda.name
+
+  policy_arn = aws_iam_policy.lambda.arn
+}
