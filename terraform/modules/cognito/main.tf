119c1a14957b8ff65295f44cf7ccdd6f5bd50334
Mohammadreza MANSOUR <56890830+mohammadrezamansour@users.noreply.github.com>
2026-06-30 23:17:40 +0200

    chore: use variable for scope

--- terraform/modules/cognito/main.tf
+++ terraform/modules/cognito/main.tf
@@ -10,35 +10,25 @@ terraform {
 }
 
 resource "aws_cognito_user_pool" "this" {
-
   name = var.config.user_pool_name
-
   tags = var.config.tags
 }
 
 resource "aws_cognito_resource_server" "this" {
-
-  identifier = var.config.resource_server_identifier
-
-  name = var.config.resource_server_name
-
+  identifier   = var.config.resource_server_identifier
+  name         = var.config.resource_server_name
   user_pool_id = aws_cognito_user_pool.this.id
 
   scope {
-
-    scope_name = "api"
-
-    scope_description = "Access Candidate API"
-
+    scope_name        = var.config.scope_name
+    scope_description = var.config.scope_description
   }
 }
 
 resource "aws_cognito_user_pool_client" "this" {
 
   name = var.config.client_name
-
   user_pool_id = aws_cognito_user_pool.this.id
-
   generate_secret = true
 
   allowed_oauth_flows = [
@@ -48,7 +38,7 @@ resource "aws_cognito_user_pool_client" "this" {
   allowed_oauth_flows_user_pool_client = true
 
   allowed_oauth_scopes = [
-    "${aws_cognito_resource_server.this.identifier}/api"
+    "${aws_cognito_resource_server.this.identifier}/${var.config.scope_name}"
   ]
 
   supported_identity_providers = [
--- terraform/modules/cognito/variables.tf
+++ terraform/modules/cognito/variables.tf
@@ -5,13 +5,11 @@ variable "config" {
   type = object({
 
     user_pool_name = string
-
     client_name = string
-
     resource_server_identifier = string
-
     resource_server_name       = string
-
+    scope_name                 = string
+    scope_description          = string
     domain = string
 
     tags = map(string)
