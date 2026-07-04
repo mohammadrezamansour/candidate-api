09098220164fe645ad7e3a1b39de074c72bd92a1
Mohammadreza MANSOUR <56890830+mohammadrezamansour@users.noreply.github.com>
2026-07-04 17:53:23 +0200

    feat: add side serverencryption and pitr and provisioned billing

--- terraform/modules/dynamodb/main.tf
+++ terraform/modules/dynamodb/main.tf
@@ -15,6 +15,10 @@ resource "aws_dynamodb_table" "this" {
   billing_mode = var.config.billing_mode
   hash_key     = var.config.hash_key
 
+  read_capacity  = var.config.billing_mode == "PROVISIONED" ? var.config.read_capacity : null
+  write_capacity = var.config.billing_mode == "PROVISIONED" ? var.config.write_capacity : null
+
+
   dynamic "attribute" {
     for_each = var.config.attributes
 
@@ -23,6 +27,18 @@ resource "aws_dynamodb_table" "this" {
       type = attribute.value.type
     }
   }
+  point_in_time_recovery {
+  enabled = var.config.enable_pitr
+}
+
+dynamic "server_side_encryption" {
+  for_each = var.config.enable_encryption ? [1] : []
+
+  content {
+    enabled     = true
+    kms_key_arn = var.config.kms_key_arn
+  }
+}
 
   tags = var.config.tags
 }
--- terraform/modules/dynamodb/variables.tf
+++ terraform/modules/dynamodb/variables.tf
@@ -5,12 +5,15 @@ variable "config" {
     table_name   = string
     billing_mode = string
     hash_key     = string
-
+    read_capacity  = optional(number)
+    write_capacity = optional(number)
     attributes = list(object({
       name = string
       type = string
     }))
-
+    enable_pitr = optional(bool, false)
+    enable_encryption = optional(bool, false)
+    kms_key_arn = optional(string)
     tags = map(string)
   })
 
@@ -23,4 +26,20 @@ validation {
 
     error_message = "Invalid billing mode"
   }
+validation {
+    condition = (
+      var.config.billing_mode == "PAY_PER_REQUEST" ||
+      (
+        var.config.billing_mode == "PROVISIONED" &&
+        var.config.read_capacity != null &&
+        var.config.write_capacity != null
+      )
+    )
+
+    error_message = "When billing_mode is PROVISIONED, read_capacity and write_capacity must be specified."
+  }
+validation {
+  condition = (!var.config.enable_encryption || var.config.kms_key_arn != null)
+  error_message = "kms arn key is required when side server encryption is enable "
+}
 }
